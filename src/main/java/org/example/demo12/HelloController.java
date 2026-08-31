472bfc4ab721db6f8269700003b7f11eddb85bad
MLobanovich <ternaer96@mail.ru>
2026-08-31 20:09:53 +0300

    Vertical win check

---
+++ src/main/java/org/example/demo12/HelloController.java
@@ -0,0 +1,66 @@
+package org.example.demo12;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+
+public class HelloController {
+
+    boolean isCross = true;
+
+    @FXML
+    protected HBox field;
+
+    @FXML
+    protected Label winLabel;
+
+    @FXML
+    protected void onButtonClick(ActionEvent event) {
+        Button button = (Button)event.getSource();
+        if (!button.getText().equals("")) {
+            return;
+        }
+        String text;
+        if (isCross) {
+            text = "X";
+        } else {
+            text = "O";
+        }
+        isCross = !isCross;
+        button.setText(text);
+        if (!isWin().equals("")) {
+            winLabel.setText("Игра окончена! Победили: " + isWin());
+            field.setDisable(true);
+        }
+    }
+
+
+    private String isWin() {
+        ArrayList<VBox> vBoxes = new ArrayList<>();
+        for (Node node : field.getChildren()) {
+            VBox vbox = (VBox)node;
+            vBoxes.add(vbox);
+        }
+
+        for (VBox vbox : vBoxes) {
+            ObservableList<Node> oneCol = vbox.getChildren(); // Получаем все кнопки
+            if (
+                    ((Button)oneCol.get(0)).getText().equals(((Button)oneCol.get(1)).getText())
+                    && ((Button)oneCol.get(0)).getText().equals(((Button)oneCol.get(2)).getText())
+            ) {
+                return ((Button)oneCol.get(0)).getText();
+            }
+        }
+
+        return "";
+    }
+
+}
